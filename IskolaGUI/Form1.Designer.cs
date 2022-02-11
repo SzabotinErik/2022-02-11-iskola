351347e5de418886a642e777a08a292bc71dd00b
SzabotinErik <szabotine.12d@acsjszki.hu>
2022-02-11 13:02:36 +0100

    9.

--- IskolaGUI/Form1.Designer.cs
+++ IskolaGUI/Form1.Designer.cs
@@ -29,13 +29,45 @@ namespace IskolaGUI
         /// </summary>
         private void InitializeComponent()
         {
+            this.btntörlés = new System.Windows.Forms.Button();
+            this.btnMentés = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // btntörlés
+            // 
+            this.btntörlés.Location = new System.Drawing.Point(31, 432);
+            this.btntörlés.Name = "btntörlés";
+            this.btntörlés.Size = new System.Drawing.Size(116, 24);
+            this.btntörlés.TabIndex = 0;
+            this.btntörlés.Text = "Törlés";
+            this.btntörlés.UseVisualStyleBackColor = true;
+            // 
+            // btnMentés
+            // 
+            this.btnMentés.Location = new System.Drawing.Point(195, 432);
+            this.btnMentés.Name = "btnMentés";
+            this.btnMentés.Size = new System.Drawing.Size(125, 24);
+            this.btnMentés.TabIndex = 1;
+            this.btnMentés.Text = "Mentés";
+            this.btnMentés.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(289, 394);
+            this.listBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(345, 489);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnMentés);
+            this.Controls.Add(this.btntörlés);
             this.Name = "Form1";
             this.Text = "IskolaGUI";
             this.ResumeLayout(false);
@@ -43,6 +75,10 @@ namespace IskolaGUI
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btntörlés;
+        private System.Windows.Forms.Button btnMentés;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
